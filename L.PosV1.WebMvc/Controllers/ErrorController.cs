7a08e5f429d45a2646e7b20445fe3fd52afe4da8
luliqzx <luliqzx@gmail.com>
2018-02-05 10:54:23 +0700

    add mvc remove web

---
+++ L.PosV1.WebMvc/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace L.PosV1.WebMvc.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult InternalServerError()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
---
+++ L.PosV1.WebMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="L.PosV1.WebMvc.MvcApplication" Language="C#" %>
